3dad3723f2e7f342cffb195ceb5986a4aea6561d
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-04 15:08:45 -0700

    set rel

--- tkinter_gui/images/images.py
+++ tkinter_gui/images/images.py
@@ -1,9 +1,15 @@
 from all_libs import *
 
-def set_logo(image):
+def set_logo(frame, image):
     logo = Image.open(image)
     logo = ImageTk.PhotoImage(logo)
-    logo_label = tk.Label(image=logo)
+    logo_label = tk.Label(frame, image=logo)
     logo_label.image = logo
     #logo_label.grid(column=2,row=0)
-    logo_label.place(relx=0.25, rely=0.2, relwidth=0.5, relheight=0.5)
+    logo_label.place(relx=0.25, rely=0.15, relwidth=0.5, relheight=0.5)
+
+def set_background(root, image):
+    background = tk.PhotoImage(image)
+    background_label = tk.Label(root, image=background)
+    #logo_label.grid(column=2,row=0)
+    background_label.place(relx=0, rely=0, relwidth=1, relheight=1)
