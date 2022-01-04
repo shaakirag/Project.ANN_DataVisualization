526de3064be421c2f5c669a106e7c94ce551f001
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-03 22:04:53 -0700

    started canvas

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import tkinter
+
 from all_libs import *
 from understanding_variables.un_var import *
+from data_cleaning.da_cl import *
+from visualization.basic_analysis import *
+from visualization.comparative_analysis import *
+from visualization.probabilistic_analysis import *
+from prediction_ann.model_ann.ann_model import *
+from prediction_ann.model_ann.ann_test import *
+from prediction_ann.model_ann.backward_propagation import *
+from prediction_ann import *
+from tkinter_gui.images import *
 
 df=pd.read_csv('diabetes.csv')
 
-print(print_head(df))
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=1000, height=600)
+canvas.grid(columnspan=5, rowspan=3)
+
+logo('tkinter_gui/images/logo.png')
+
+root.mainloop()
+
