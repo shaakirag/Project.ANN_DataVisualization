80e8dc77ec6ee4601f90e2a3fc4f77112ad29634
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-03 21:30:21 -0700

    added count_plot

--- visualization/basic_analysis.py
+++ visualization/basic_analysis.py
@@ -1,8 +1,16 @@
 from all_libs import *
 
 # Count Plot
-def count_plot(df, header_list):
-    pass
+def count_plot(df, label):
+    f, ax = plt.pyplot.subplots(1,2,figsize=(10,5))
+    df[label].value_counts().plot.pie(explode=[0,0.1],autopct='%1.1f%%', ax=ax[0], shadow=True)
+    ax[0].set_title(label)
+    ax[0].set_ylabel('')
+    sns.countplot(label, data=df, ax=ax[1])
+    ax[1].set_title(label)
+    N,P = df[label].value_counts()
+    plt.pyplot.grid()
+    plt.pyplot.show()
 
 #  Histograms
 def count_histogram(df, header_list):
