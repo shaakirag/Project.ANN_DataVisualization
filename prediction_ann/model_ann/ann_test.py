93f341a76d8ebc9a4533f01eac372e2b3a3506d7
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-03 20:36:08 -0700

    added functions

--- prediction_ann/model_ann/ann_test.py
+++ prediction_ann/model_ann/ann_test.py
@@ -1,2 +1,22 @@
 from all_libs import *
 from prediction_ann import *
+
+def test_predictions(model, X_test):
+    predictions=[]
+    with torch.no_grad():
+        for i,data in enumerate(X_test):
+            y_pred=model(data)
+            predictions.append(y_pred.argmax().item())
+    return predictions
+
+# Confusion Matrix
+def con_matrix(y_test, predictions):
+    cm = confusion_matrix(y_test,predictions)
+    plt.pyplot.figure(figsize=(10,6))
+    sns.heatmap(cm,annot=True)
+    plt.pyplot.xlabel('Actual Values')
+    plt.pyplot.ylabel('Predicted Values')
+    plt.pyplot.show()
+
+def acc_score(y_test, predictions):
+    return accuracy_score(y_test,predictions)
