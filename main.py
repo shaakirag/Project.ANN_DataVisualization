0f24ed0987c49b467c5b1a68a66ee202c638e88d
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-04 15:08:30 -0700

    menubar

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tkinter
+
 from all_libs import *
 from understanding_variables.un_var import *
 from data_cleaning.da_cl import *
@@ -21,17 +23,32 @@ root = tk.Tk()
 
 
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
-canvas.grid(columnspan=5, rowspan=5)
+canvas.pack()
 
-frame = tk.Frame(root)
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.9)
+# Welcome Page
 
-set_logo('tkinter_gui/images/logo.png')
+#Does not work
+#set_background(root, 'tkinter_gui/images/background.png')
 
-# Welcome message
-welcome = tk.Label(frame, text = 'Upload a dataset to continue', font = font)
-#welcome.grid(columnspan=1, column=2, row=1)
-welcome.place(relx=0.3, rely=0.7, relwidth=0.4, relheight=0.05)
+frame = tk.Frame(root, bg = 'White', bd=5)
+frame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+set_logo(frame, 'tkinter_gui/images/logo.png')
+
+# Menu
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+
+filemenu.add_command(label="Add Labels", command=lambda:add_label(font))
+menubar.add_cascade(label="Actions", menu=filemenu)
+
+root.config(menu=menubar)
+
+# Instructions message
+instructions_txt = tk.StringVar()
+instructions_btn = tk.Button(frame, textvariable=instructions_txt, command=lambda:instructions(font), font=font, bg='White', fg='Black', height=2, width=15)
+instructions_txt.set('Instructions')
+instructions_btn.place(relx=0.325, rely=0.7, relwidth=0.35, relheight=0.05)
 
 # Browse button
 browse_txt = tk.StringVar()
@@ -39,5 +56,10 @@ browse_btn = tk.Button(frame, textvariable=browse_txt, command=lambda:open_file(
 browse_txt.set('Browse')
 #browse_btn.grid(column=2, row=3)
 browse_btn.place(relx=0.425, rely=0.77, relwidth=0.15, relheight=0.05)
+
+
+close = tk.Button(root, text='Close', command=root.destroy, font=font, bg='Black', fg='White')
+close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
+
 
 root.mainloop()
--- tkinter_gui/funcs.py
+++ tkinter_gui/funcs.py
@@ -1,5 +1,26 @@
+import tkinter
+
 from all_libs import *
 
+# Instructions pop up
+def instructions(font):
+    inst = tk.Toplevel()
+    inst.title('Instructions')
+    frame = tk.Frame(inst, bg='White', bd=5)
+    frame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+    # Text box for instructions
+    frame1 = tk.LabelFrame(frame, text='Instructions', font=font)
+    frame1.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+    instructions = tk.Label(frame1, text="Super long text",
+                         font=font)
+    instructions.pack()
+
+    close = tk.Button(inst, text='Close', command=inst.destroy, font=font, bg='#008080', fg='White')
+    close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
+
+# Uploading the file
 def open_file(root, frame, browse_txt, data):
     browse_txt.set('Loading...')
     while True:
@@ -17,3 +38,24 @@ def open_file(root, frame, browse_txt, data):
     #file_name_box.grid(column=2, row=4)
     file_name_box.place(relx=0.3, rely=0.85, relwidth=0.4, relheight=0.04)
     browse_txt.set('Browse')
+
+# Adding Labels
+def add_label(font):
+    label = tk.Toplevel()
+    label.title('Add Labels')
+    frame = tk.Frame(label, bg='White', bd=5)
+    frame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+
+    close = tk.Button(label, text='Close', command=label.destroy, font=font, bg='#008080', fg='White')
+    close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
+
+
+
+
+
+
+
+
+
+
