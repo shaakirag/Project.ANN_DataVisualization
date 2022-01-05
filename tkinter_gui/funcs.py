653517453d816a2015dbc481bffa169c105b3b48
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-04 21:15:44 -0700

    understanding vars fn

--- tkinter_gui/funcs.py
+++ tkinter_gui/funcs.py
@@ -1,7 +1,5 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfile
 import os
-from understanding_variables import *
+from understanding_variables.un_var import *
 
 # Instructions pop up
 def instructions(font):
@@ -23,13 +21,13 @@ def instructions(font):
 
 
 # Uploading the file
-def open_file(root, frame, menubar, filemenu, editmenu, browse_txt, data, font):
+def open_file(root, frame, menubar, filemenu, editmenu, browse_txt, df, font):
     browse_txt.set('Loading...')
     while True:
-        file = askopenfile(parent=root, mode='rb', title='Choose a file', filetype=[('csv file', '*.csv')])
+        file = tk.filedialog.askopenfile(parent=root, mode='rb', title='Choose a file', filetype=[('csv file', '*.csv')])
         if file:
-            while not isinstance(data, pd.DataFrame):
-                data = pd.read_csv(file)
+            while not isinstance(df, pd.DataFrame):
+                df = pd.read_csv(file)
             break
 
     # Small text box under browse button
@@ -42,17 +40,17 @@ def open_file(root, frame, menubar, filemenu, editmenu, browse_txt, data, font):
 
     browse_txt.set('Browse')
 
-    set_menu_label = tk.Button(frame, text='Next', command=lambda:set_label_menu(menubar, filemenu, editmenu, font), font=font, bg='#008080', fg='White')
+    set_menu_label = tk.Button(frame, text='Next', command=lambda:set_label_menu(menubar, filemenu, editmenu, font, df), font=font, bg='#008080', fg='White')
     set_menu_label.place(relx=0.9, rely=0.95, relwidth=0.1, relheight=0.04)
 
-def set_label_menu(menubar, filemenu, editmenu, font):
+def set_label_menu(menubar, filemenu, editmenu, font, df):
     if filemenu.index('end') is not None:
         for i in range(filemenu.index('end') + 1):
             if filemenu.entrycget(i, 'label') == 'Add Labels':
                 filemenu.delete('Add Labels')
                 menubar.delete('Actions')
 
-    filemenu.add_command(label="Add Labels", command=lambda: add_label(menubar, editmenu, font))
+    filemenu.add_command(label="Add Labels", command=lambda: add_label(menubar, editmenu, font, df))
     menubar.add_cascade(label="Actions", menu=filemenu)
 
 class Label:
@@ -65,7 +63,7 @@ class Label:
 
 
 # Adding Labels
-def add_label(menubar, editmenu, font):
+def add_label(menubar, editmenu, font, df):
     label = tk.Toplevel()
     label.title('Add Labels')
     frame = tk.Frame(label, bg='White', bd=5)
@@ -94,13 +92,13 @@ def add_label(menubar, editmenu, font):
     set_values = tk.Button(frame, text='Set Values', command=lambda: set_value_fields([[label_entry, label_entry_outcome],
                                                                           [label_0_entry, label_0_entry_negative],
                                                                           [label_1_entry, label_1_entry_positive]],
-              menubar, editmenu, font), font=font, bg='#008080', fg='White')
+              menubar, editmenu, font, df), font=font, bg='#008080', fg='White')
     set_values.place(relx=0.4, rely=0.9, relwidth=0.2,relheight=0.04)
 
     close = tk.Button(label, text='Close', command=label.destroy, font=font, bg='#008080', fg='White')
     close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
 
-def set_value_fields(labels, menubar, editmenu, font):
+def set_value_fields(labels, menubar, editmenu, font, df):
     for l in labels:
         l[1].label_func(l[0])
 
@@ -111,23 +109,27 @@ def set_value_fields(labels, menubar, editmenu, font):
                 editmenu.delete('Understanding Your Variables')
                 menubar.delete('Pre-Processing')
 
-    editmenu.add_command(label="Understanding Your Variables", command=lambda: understanding_your_variables())
+    editmenu.add_command(label="Understanding Your Variables", command=lambda: understanding_your_variables(font, df))
     editmenu.add_command(label="Cleaning Data", command=lambda: add_label(menubar, font))
     menubar.add_cascade(label='Pre-Processing', menu=editmenu)
 
-def understanding_your_variables():
+def understanding_your_variables(font, df):
     vars = tk.Toplevel()
     vars.title('Understanding Your Variables')
-    frame = tk.Frame(vars='White', bd=5)
+    frame = tk.Frame(vars, bg='White', bd=5)
     frame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
 
-    tk.Label(frame, text="Label", font=font).place(relx=0.4, rely=0.2, relwidth=0.2, relheight=0.1)
-    tk.Label(frame, text="Label 0", font=font).place(relx=0.4, rely=0.4, relwidth=0.2, relheight=0.1)
-    tk.Label(frame, text="Label 1", font=font).place(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='Head', command=lambda:head(df), font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.05, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='Tail', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.16, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='Sample', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.27, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='Row and Column Number', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.38, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='Column Types', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.49, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='NULL Info', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='Mean per Label', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.71, relwidth=0.2, relheight=0.1)
+    tk.Button(frame, text='Statistical Summary', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.82, relwidth=0.2, relheight=0.1)
 
-    label_entry = tk.Entry(frame, font=font)
-    label_0_entry = tk.Entry(frame, font=font)
-    label_1_entry = tk.Entry(frame, font=font)
+    close = tk.Button(vars, text='Close', command=vars.destroy, font=font, bg='#008080', fg='White')
+    close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
 
 
 
