6494193b34ef9bba6fa160357487d02b7705edb8
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-05 14:25:40 -0700

    tail and sample

--- tkinter_gui/funcs.py
+++ tkinter_gui/funcs.py
@@ -9,8 +9,6 @@ def instructions(main_notebook, font):
 
     main_notebook.add(frame, text='Instructions')
 
-    main_notebook.select(1)
-
     # Text box for instructions
     frame1 = tk.LabelFrame(frame, text='Instructions', font=font)
     frame1.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
@@ -70,17 +68,18 @@ def add_label(main_notebook, menubar, editmenu, font, df):
     frame = tk.Frame(main_notebook, bg='White', width=600, height=600, bd=5)
     frame.pack(fill='both', expand=1)
 
-    main_notebook.add(frame, text='Add Labels')
+    main_notebook.add(frame, text='Labels')
 
-    main_notebook.select(1)
+    frame1 = tk.LabelFrame(frame, text='Instructions', font=font)
+    frame1.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
 
-    tk.Label(frame, text="Label", font=font).place(relx=0.4, rely=0.2, relwidth=0.2, relheight=0.1)
-    tk.Label(frame, text="Label 0", font=font).place(relx=0.4, rely=0.4, relwidth=0.2, relheight=0.1)
-    tk.Label(frame, text="Label 1", font=font).place(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
+    tk.Label(frame1, text="Label", font=font).place(relx=0.4, rely=0.2, relwidth=0.2, relheight=0.1)
+    tk.Label(frame1, text="Label 0", font=font).place(relx=0.4, rely=0.4, relwidth=0.2, relheight=0.1)
+    tk.Label(frame1, text="Label 1", font=font).place(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
 
-    label_entry = tk.Entry(frame, font=font)
-    label_0_entry = tk.Entry(frame, font=font)
-    label_1_entry = tk.Entry(frame, font=font)
+    label_entry = tk.Entry(frame1, font=font)
+    label_0_entry = tk.Entry(frame1, font=font)
+    label_1_entry = tk.Entry(frame1, font=font)
 
     label_entry.insert(10, "Outcome")
     label_0_entry.insert(10, "Negative")
@@ -94,7 +93,7 @@ def add_label(main_notebook, menubar, editmenu, font, df):
     label_0_entry_negative = Label('Negative')
     label_1_entry_positive = Label('Positive')
 
-    set_values = tk.Button(frame, text='Set Values', command=lambda: set_value_fields([[label_entry, label_entry_outcome],
+    set_values = tk.Button(frame1, text='Set Values', command=lambda: set_value_fields(main_notebook, [[label_entry, label_entry_outcome],
                                                                           [label_0_entry, label_0_entry_negative],
                                                                           [label_1_entry, label_1_entry_positive]],
               menubar, editmenu, font, df), font=font, bg='#008080', fg='White')
@@ -103,7 +102,7 @@ def add_label(main_notebook, menubar, editmenu, font, df):
     close = tk.Button(frame, text='Close', command=frame.destroy, font=font, bg='#008080', fg='White')
     close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
 
-def set_value_fields(labels, menubar, editmenu, font, df):
+def set_value_fields(main_notebook, labels, menubar, editmenu, font, df):
     for l in labels:
         l[1].label_func(l[0])
 
@@ -114,26 +113,29 @@ def set_value_fields(labels, menubar, editmenu, font, df):
                 editmenu.delete('Understanding Your Variables')
                 menubar.delete('Pre-Processing')
 
-    editmenu.add_command(label="Understanding Your Variables", command=lambda: understanding_your_variables(font, df))
+    editmenu.add_command(label="Understanding Your Variables", command=lambda: understanding_your_variables(main_notebook, font, df))
     editmenu.add_command(label="Cleaning Data", command=lambda: add_label(menubar, font))
     menubar.add_cascade(label='Pre-Processing', menu=editmenu)
 
-def understanding_your_variables(font, df):
-    vars = tk.Toplevel()
-    vars.title('Understanding Your Variables')
-    frame = tk.Frame(vars, bg='White', bd=5)
-    frame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+def understanding_your_variables(main_notebook, font, df):
+    frame = tk.Frame(main_notebook, bg='White', width=600, height=600, bd=5)
+    frame.pack(fill='both', expand=1)
 
-    tk.Button(frame, text='Head', command=lambda:head(df, font), font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.05, relwidth=0.2, relheight=0.1)
-    tk.Button(frame, text='Tail', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.16, relwidth=0.2, relheight=0.1)
-    tk.Button(frame, text='Sample', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.27, relwidth=0.2, relheight=0.1)
-    tk.Button(frame, text='Row and Column Number', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.38, relwidth=0.2, relheight=0.1)
-    tk.Button(frame, text='Column Types', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.49, relwidth=0.2, relheight=0.1)
-    tk.Button(frame, text='NULL Info', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
-    tk.Button(frame, text='Mean per Label', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.71, relwidth=0.2, relheight=0.1)
-    tk.Button(frame, text='Statistical Summary', font=font, bg='#008080', fg='White').place(relx=0.4, rely=0.82, relwidth=0.2, relheight=0.1)
+    main_notebook.add(frame, text='Understanding Your Variables')
 
-    close = tk.Button(vars, text='Close', command=vars.destroy, font=font, bg='#008080', fg='White')
+    frame1 = tk.LabelFrame(frame, text='Select a Command', font=font)
+    frame1.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+    tk.Button(frame1, text='Head', command=lambda:head(main_notebook, df, font), font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.05, relwidth=0.5, relheight=0.05)
+    tk.Button(frame1, text='Tail', command=lambda:tail(main_notebook, df, font), font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.16, relwidth=0.5, relheight=0.05)
+    tk.Button(frame1, text='Sample', command=lambda:sample(main_notebook, df, font), font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.27, relwidth=0.5, relheight=0.05)
+    tk.Button(frame1, text='Row and Column Number', font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.38, relwidth=0.5, relheight=0.05)
+    tk.Button(frame1, text='Column Types', font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.49, relwidth=0.5, relheight=0.05)
+    tk.Button(frame1, text='NULL Info', font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.6, relwidth=0.5, relheight=0.05)
+    tk.Button(frame1, text='Mean per Label', font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.71, relwidth=0.5, relheight=0.05)
+    tk.Button(frame1, text='Statistical Summary', font=font, bg='#008080', fg='White').place(relx=0.225, rely=0.82, relwidth=0.5, relheight=0.05)
+
+    close = tk.Button(frame, text='Close', command=frame.destroy, font=font, bg='#008080', fg='White')
     close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
 
 
--- understanding_variables/un_var.py
+++ understanding_variables/un_var.py
@@ -1,36 +1,93 @@
 import tkinter as tk
+import matplotlib
+matplotlib.use("TKAgg")
 
 # Print the first few rows
-def head(df, font):
-    head_ = tk.Toplevel()
-    head_.title('Head')
-    frame = tk.Frame(head_, bg='White', bd=5)
-    frame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+def head(main_notebook, df, font):
+    frame = tk.Frame(main_notebook, bg='White', width=600, height=600, bd=5)
+    frame.pack(fill='both', expand=1)
 
-    tk.Label(frame, text="No. of Rows", font=font).place(relx=0.4, rely=0.2, relwidth=0.2, relheight=0.1)
-    head_entry = tk.Entry(frame, font=font)
+    main_notebook.add(frame, text='_Head')
+
+    frame1 = tk.LabelFrame(frame, text='Set Input', font=font)
+    frame1.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+    tk.Label(frame1, text="No. of Rows", font=font).place(relx=0.4, rely=0.2, relwidth=0.2, relheight=0.1)
+    head_entry = tk.Entry(frame1, font=font)
     head_entry.insert(10, 5)
     head_entry.place(relx=0.4, rely=0.3, relwidth=0.2, relheight=0.04)
 
-    set_head = tk.Button(frame, text='Set Value',
-                           command=lambda: set_head_field(head_entry), font=font, bg='#008080',fg='White')
+    set_head = tk.Button(frame1, text='Set Value',
+                           command=lambda: set_head_field(head_entry, frame1, df, font), font=font, bg='#008080',fg='White')
     set_head.place(relx=0.4, rely=0.4, relwidth=0.2, relheight=0.04)
 
-    close = tk.Button(head_, text='Close', command=head_.destroy, font=font, bg='#008080', fg='White')
-    close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
-    return df.head()
 
-def set_head_field(head_entry):
-    rows = head_entry.get()
-    print(rows)
+    close = tk.Button(frame, text='Close', command=frame.destroy, font=font, bg='#008080', fg='White')
+    close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
+
+def set_head_field(head_entry, frame1, df, font):
+    rows = int(head_entry.get())
+    head_ = df.head(rows)
+    output = tk.Label(frame1, text=head_, font=(font, 8))
+    output.place(relx=0, rely=0.5, relwidth=1, relheight=0.5)
 
 # Print the last few rows
-def tail(df):
-    return df.tail()
+def tail(main_notebook, df, font):
+    frame = tk.Frame(main_notebook, bg='White', width=600, height=600, bd=5)
+    frame.pack(fill='both', expand=1)
+
+    main_notebook.add(frame, text='_Tail')
+
+    frame1 = tk.LabelFrame(frame, text='Set Input', font=font)
+    frame1.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+    tk.Label(frame1, text="No. of Rows", font=font).place(relx=0.4, rely=0.2, relwidth=0.2, relheight=0.1)
+    tail_entry = tk.Entry(frame1, font=font)
+    tail_entry.insert(10, 5)
+    tail_entry.place(relx=0.4, rely=0.3, relwidth=0.2, relheight=0.04)
+
+    set_head = tk.Button(frame1, text='Set Value',
+                           command=lambda: set_tail_field(tail_entry, frame1, df, font), font=font, bg='#008080',fg='White')
+    set_head.place(relx=0.4, rely=0.4, relwidth=0.2, relheight=0.04)
+
+
+    close = tk.Button(frame, text='Close', command=frame.destroy, font=font, bg='#008080', fg='White')
+    close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
+
+def set_tail_field(tail_entry, frame1, df, font):
+    rows = int(tail_entry.get())
+    tail_ = df.tail(rows)
+    output = tk.Label(frame1, text=tail_, font=(font, 8))
+    output.place(relx=0, rely=0.5, relwidth=1, relheight=0.5)
 
 # Display a random number of records
-def sample(df,n):
-    return df.sample(n)
+def sample(main_notebook, df, font):
+    frame = tk.Frame(main_notebook, bg='White', width=600, height=600, bd=5)
+    frame.pack(fill='both', expand=1)
+
+    main_notebook.add(frame, text='_Sample')
+
+    frame1 = tk.LabelFrame(frame, text='Set Input', font=font)
+    frame1.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.9)
+
+    tk.Label(frame1, text="No. of Rows", font=font).place(relx=0.4, rely=0.2, relwidth=0.2, relheight=0.1)
+    sample_entry = tk.Entry(frame1, font=font)
+    sample_entry.insert(10, 5)
+    sample_entry.place(relx=0.4, rely=0.3, relwidth=0.2, relheight=0.04)
+
+    set_head = tk.Button(frame1, text='Set Value',
+                           command=lambda: set_sample_field(sample_entry, frame1, df, font), font=font, bg='#008080',fg='White')
+    set_head.place(relx=0.4, rely=0.4, relwidth=0.2, relheight=0.04)
+
+
+    close = tk.Button(frame, text='Close', command=frame.destroy, font=font, bg='#008080', fg='White')
+    close.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.04)
+
+def set_sample_field(sample_entry, frame1, df, font):
+    rows = int(sample_entry.get())
+    sample_ = df.tail(rows)
+    output = tk.Label(frame1, text=sample_, font=(font, 8))
+    output.place(relx=0, rely=0.5, relwidth=1, relheight=0.5)
 
 # View the number of rows and columns
 def shape(df):
