c0268429b7b73527e6ebcf4f4ea34cbeb781f8d8
Shaakira Gadiwan <shaakira.gadiwan@ucalgary.ca>
2022-01-04 20:37:13 -0700

    fixed error

--- prediction_ann/pre_ann.py
+++ prediction_ann/pre_ann.py
@@ -3,4 +3,4 @@ from prediction_ann import *
 
 def prediction(model, new_data):
     new_data = torch.tensor(new_data)
-        return model(new_data).argmax().item()
+    return model(new_data).argmax().item()
